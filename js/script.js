abe06dd078d8a9abad6f9e1a639a6d9908244e5f
Andrey2220 <konovalenkoandrii7@gmail.com>
2024-03-19 06:53:00 +0100

    v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-const audio = 
-document.querySelector('audio');
-const body = document.body
+const { body, querySelector } = document;
+const { audio } = querySelector('audio');
 
 audio.addEventListener('play', () => {
     body.classList.add('play-music');
